65c922d1d4eacecb655fec26c3ae21ddb894b7db
RL-004 <rl-004@RL-004s-iMac.local>
2016-12-06 16:29:58 +0800

    finish print

--- src/pagereplacementpolicies/PageReplacementPolicies.java
+++ src/pagereplacementpolicies/PageReplacementPolicies.java
@@ -5,7 +5,7 @@
  */
 package pagereplacementpolicies;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -35,23 +35,68 @@ public class PageReplacementPolicies {
 //            System.out.println("Length of the REFERENCE STRING: ");
 //            size = scanner.nextInt();
 //            System.out.println("REFERENCE STRING: ");
+            
+            choice = 1;
+            
+            if(choice == 1){
+                fifo(refString, frames);
+            }
+            else if (choice == 2){
+                lru(refString, frames);
+            }
+            else if(choice == 3){
+                lfu(refString, frames);
+            }
 //        }
         
         
 //        fifo(refString, frames);
 //        lru(refString, frames);
-        lfu(refString, frames);
+//        lfu(refString, frames);
     }
     
+    
+    static void toString(String[][] tables, int row, int column, String[] tally){
+        StringBuilder sb = new StringBuilder();
+        
+        for(int r = 0; r < row; r++){
+            for (int c = 0; c < column; c++){
+                String s = "[" + tables[r][c] + "]";
+                sb.append(s).append(" ");
+            }
+            
+            sb.append("\n");
+        }
+        
+        for(String item : tally)
+            sb.append(item).append(" ");
+        
+        System.out.println(sb.toString());
+    }
+    
+    
+    static String[][] populate(String[][] table, Object[] state, int index){
+        
+        for(int i = 0; i < state.length; i++){
+            
+            table[i][index] = String.valueOf(state[i]);
+        }
+        
+        return table;
+    }
+    
+    
     static void fifo(int[] refString, int frames){
-        Queue queue = new LinkedList();
-        ArrayList<Object[]> toPrint = new ArrayList<>();
+        Queue queue = new LinkedList(Arrays.asList(" ", " ", " "));
         String [] tally = new String[refString.length];
+        
+        String[][] toPrint = new String[frames][refString.length];
         int count = 0;
         
         for(int i = 0; i < refString.length; i++){
+            
             System.out.println(queue.toString());
-            toPrint.add(queue.toArray());
+            toPrint = populate(toPrint, queue.toArray(), i);
             
             if(queue.contains(refString[i]) == false){
                 System.out.println("Page fault: memory reference " + refString[i] + ": not found.");
@@ -61,12 +106,15 @@ public class PageReplacementPolicies {
                     queue.remove();
                     
                 queue.add(refString[i]);
-                tally[i] = "x";
+                tally[i] = "-F-";
             }
-            else
-                tally[i] = "";
+            else{
+                tally[i] = "-H-";
+            }
+            
         }
         
+        toString(toPrint, frames, refString.length, tally);
         System.out.println("Page Fault count: " + count);
     }
     
